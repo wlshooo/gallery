e4e3407dea75916655b096ff0bc5298e0f8361d0
jinhoLee <kswlsgh@naver.com>
2023-08-25 03:25:10 +0900

    feat : 8. 로그인기능

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Header/>
-  <Home/>
+  <RouterView/>
   <Footer/>
 
 
@@ -10,13 +10,11 @@
 <script>
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Home from "@/pages/Home";
 export default {
   name: 'App',
   components: {
     Header,
     Footer,
-    Home
   }
 }
 </script>
