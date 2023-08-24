2359d3186c5290d990c15d1c45937b469d428037
jinhoLee <kswlsgh@naver.com>
2023-08-24 23:20:41 +0900

    feat : 6.상품 진열 및 DB column 추가:

--- backend/src/main/java/org/africalib/gallery/backend/entity/Item.java
+++ backend/src/main/java/org/africalib/gallery/backend/entity/Item.java
@@ -15,4 +15,13 @@ public class Item {
 
     @Column(length = 50, nullable = false)
     private String name;
+
+    @Column(length = 100, nullable = false)
+    private String imgPath;
+
+    @Column
+    private int discountPer;
+
+    @Column
+    private int price;
 }
